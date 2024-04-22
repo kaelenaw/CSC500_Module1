f385470d86e949a271ff42e51a60722805fbb8fe
kaelenaw <167730833+kaelenaw@users.noreply.github.com>
2024-04-21 22:13:26 -0600

    Add files via upload

---
+++ NumAddSub.py
@@ -0,0 +1,13 @@
+print()
+print('Welcome to the Amazing Addition/Subtraction Machine!','\n')
+
+num1 = int(input('Enter first number:'))
+print('You entered:', num1, '\n')
+
+num2 = int(input('Enter second number:'))
+print('You entered:', num2, '\n')
+
+print(num1, 'plus', num2, 'is', num1 + num2)
+print(num1, 'minus', num2, 'is', num1 - num2, '\n')
+
+print('Thank you for playing!')
---
+++ NumMultDiv.py
@@ -0,0 +1,13 @@
+print()
+print('Welcome to the Amazing Multiplication/Division Machine!','\n')
+
+num1 = int(input('Enter first number:'))
+print('You entered:', num1, '\n')
+
+num2 = int(input('Enter second number:'))
+print('You entered:', num2, '\n')
+
+print(num1, 'multiplied by', num2, 'is', num1 * num2)
+print(num1, 'divided by', num2, 'is', num1 / num2, '\n')
+
+print('Thank you for playing!')
